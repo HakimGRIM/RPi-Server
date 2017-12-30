5b055fea0f78ed46154a636e7459cfa973634945
Hakim_GRIM <hakimgrim.info@gmail.com>
2017-12-30 19:48:04 +0100

    Modify test.py code

--- RPi-Server/test.py
+++ RPi-Server/test.py
@@ -1,10 +1,13 @@
 import RPi.GPIO as GPIO 
 import time
- 
-GPIO.setmode(GPIO.BCM) 
+
+GPIO.setwarnings(False) 
+GPIO.setmode(GPIO.BCM)
+
 GPIO.setup(25, GPIO.OUT)
 GPIO.setup(7, GPIO.OUT)
 GPIO.setup(8, GPIO.OUT)
+
 GPIO.output(7, GPIO.LOW)
 GPIO.output(7, GPIO.HIGH)
  
