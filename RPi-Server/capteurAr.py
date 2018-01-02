6e9e36340279c23f923ec24ac96dfa201ee06684
Hakim_GRIM <hakimgrim.info@gmail.com>
2018-01-02 16:42:31 +0100

    DD

--- RPi-Server/capteurAr.py
+++ RPi-Server/capteurAr.py
@@ -17,20 +17,22 @@ class Arriere(Thread):
         GPIO.setup(self.ECHO, GPIO.IN)
     
     def run(self):
-        GPIO.output(self.TRIG, False)
-        time.sleep(2)
-        GPIO.output(self.TRIG, True)
-        time.sleep(0.00001)
-        GPIO.output(self.TRIG, False)
-        while GPIO.input(self.ECHO) == 0:
-            pulse_start = time.time()
-        while GPIO.input(self.ECHO) == 1:
-            pulse_end = time.time()
-        pulse_duration = pulse_end - pulse_start
-        self.distance = pulse_duration * 17150
-        self.distance = round(self.distance, 2)
-        print "Distance: ", self.distance, " cm"
-        GPIO.cleanup()
+        while 1:
+            GPIO.output(self.TRIG, False)
+            time.sleep(2)
+            GPIO.output(self.TRIG, True)
+            time.sleep(0.00001)
+            GPIO.output(self.TRIG, False)
+            
+            while GPIO.input(self.ECHO) == 0:
+                pulse_start = time.time()
+            while GPIO.input(self.ECHO) == 1:
+                pulse_end = time.time()
+            pulse_duration = pulse_end - pulse_start
+            self.distance = pulse_duration * 17150
+            self.distance = round(self.distance, 2)
+            print "Distance: ", self.distance, " cm"
+            GPIO.cleanup()
 
     def result(self):
         return self.distance
