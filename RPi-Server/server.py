d2217a7dfd685e9535219cca7fbe89fb39c1597c
Hakim_GRIM <hakimgrim.info@gmail.com>
2018-01-02 14:56:03 +0100

    DD

--- RPi-Server/server.py
+++ RPi-Server/server.py
@@ -41,7 +41,7 @@ for pin in var_ar:
 	GPIO.setup(pin, GPIO.OUT)
 #------------------------------------------------------------------------------------------------------#
 #--Lancement des thread pour les capteur sonor--#
-th_sonor_ar = Arriere()
+th_sonsor_ar = Arriere()
 th_sonsor_ar.start()
 #------------------------------------------------------------------------------------------------------#
 #--Definition des fonction de commande--#
