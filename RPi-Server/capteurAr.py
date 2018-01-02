dd512b85a134711254dd7776fe70715eea496937
Hakim_GRIM <hakimgrim.info@gmail.com>
2018-01-02 19:45:45 +0100

    DD

--- RPi-Server/capteurAr.py
+++ RPi-Server/capteurAr.py
@@ -15,25 +15,29 @@ class Arriere(Thread):
         self.distance = 0
     
     def run(self):
-        while 1:
-            GPIO.setmode(GPIO.BCM)
-            GPIO.setup(self.TRIG, GPIO.OUT)
-            GPIO.setup(self.ECHO, GPIO.IN)
-            GPIO.output(self.TRIG, False)
-            time.sleep(2)
-            GPIO.output(self.TRIG, True)
-            time.sleep(0.00001)
-            GPIO.output(self.TRIG, False)
-            
-            while GPIO.input(self.ECHO) == 0:
-                pulse_start = time.time()
-            while GPIO.input(self.ECHO) == 1:
-                pulse_end = time.time()
-            pulse_duration = pulse_end - pulse_start
-            self.distance = pulse_duration * 17150
-            self.distance = round(self.distance, 2)
-            print "Distance: ", self.distance, " cm"
-            GPIO.cleanup()
+        try:
+            while 1:
+                GPIO.setmode(GPIO.BCM)
+                GPIO.setup(self.TRIG, GPIO.OUT)
+                GPIO.setup(self.ECHO, GPIO.IN)
+                GPIO.output(self.TRIG, False)
+                time.sleep(2)
+                GPIO.output(self.TRIG, True)
+                time.sleep(0.00001)
+                GPIO.output(self.TRIG, False)
+                
+                while GPIO.input(self.ECHO) == 0:
+                    pulse_start = time.time()
+                while GPIO.input(self.ECHO) == 1:
+                    print ("ggggggggggggggggggggggg")
+                    pulse_end = time.time()
+                pulse_duration = pulse_end - pulse_start
+                self.distance = pulse_duration * 17150
+                self.distance = round(self.distance, 2)
+                print "Distance: ", self.distance, " cm"
+                GPIO.cleanup()
+            except KeyboardInterrupt:
+                pass
 
     def result(self):
         return self.distance
