5a4ccc3305e5a82db297877b9376a120a37e0f61
Hakim_GRIM <hakimgrim.info@gmail.com>
2018-01-02 14:58:03 +0100

    DD

--- RPi-Server/capteurAr.py
+++ RPi-Server/capteurAr.py
@@ -14,18 +14,18 @@ class Arriere(Thread):
         self.ECHO = 31
     
     def run(self):
-        GPIO.output(TRIG, False)
+        GPIO.output(self.TRIG, False)
         print ("Distance Measurement In Progress")
-        GPIO.setup(TRIG, GPIO.OUT)
-        GPIO.setup(ECHO, GPIO.IN)
+        GPIO.setup(self.TRIG, GPIO.OUT)
+        GPIO.setup(self.ECHO, GPIO.IN)
         print ("Waiting For Sensor To Settle")
         time.sleep(2)
-        GPIO.output(TRIG, True)
+        GPIO.output(self.TRIG, True)
         time.sleep(0.00001)
-        GPIO.output(TRIG, False)
-        while GPIO.input(ECHO) == 0:
+        GPIO.output(self.TRIG, False)
+        while GPIO.input(self.ECHO) == 0:
             pulse_start = time.time()
-        while GPIO.input(ECHO) == 1:
+        while GPIO.input(self.ECHO) == 1:
             pulse_end = time.time()
         pulse_duration = pulse_end - pulse_start
         distance = pulse_duration * 17150
